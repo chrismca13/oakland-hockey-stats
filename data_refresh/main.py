f15465a58085f7878478db5066a4eea03325443e
chrismca13 <chrismca@umich.edu>
2025-09-02 18:39:13 -0700

    Initial repo commit

---
+++ data_refresh/main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from helpers import upload_df_to_s3, initial_web_data, update_current_season, data_manip
+
+# Load config.json
+import json
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+TOTAL_REFRESH = config["TOTAL_REFRESH"]
+SEASON_DIM_PATH = config["SEASON_DIM_PATH"]
+CURRENT_BUCKET_PATH = config["CURRENT_BUCKET_PATH"]
+NEW_FILE_NAME = config["NEW_FILE_NAME"]
+DIVISION_DICT = config["DIVISION_DICT"]
+BUCKET_NAME = config['BUCKET_NAME']
+
+
+def main():
+
+    season_dim = pd.read_csv(SEASON_DIM_PATH)
+    if TOTAL_REFRESH:
+        data = initial_web_data(season_dim, CURRENT_BUCKET_PATH, NEW_FILE_NAME, DIVISION_DICT, BUCKET_NAME)
+
+    # Run this even if TOTAL_REFRESH == TRUE so it drops data in the config's designated S3 path
+    data = update_current_season(CURRENT_BUCKET_PATH, DIVISION_DICT)
+
+    df = data_manip(data, season_dim)
+    upload_df_to_s3(df, BUCKET_NAME, NEW_FILE_NAME)
+    
+if __name__ == "__main__":
+    main()
