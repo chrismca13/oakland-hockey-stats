835b3d4655ba2b5363f5378902e2f5f9ef3fcb3c
chrismca13 <chrismca@umich.edu>
2025-10-27 20:13:26 -0700

    Add multiple refreshes for streamlit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 import json
+from datetime import datetime
+
 
 from data_refresh.helpers import upload_df_to_s3, update_current_season, data_manip, initial_web_data
 
@@ -19,6 +21,11 @@ BUCKET_NAME = config['BUCKET_NAME']
 print('opened config and loaded vars')
 
 def main():
+    now = datetime.now()
+    # weekday() == 2 means Wednesday (Monday=0, ..., Sunday=6)
+    if not (now.weekday() == 2 and now.hour < 12):
+        print("Script is only allowed to run before noon on Wednesday. Exiting.")
+        return
 
     print('reading in season_dim')
     season_dim = pd.read_csv(SEASON_DIM_PATH)
@@ -26,7 +33,6 @@ def main():
         data = initial_web_data(season_dim, CURRENT_BUCKET_PATH, NEW_FILE_NAME, DIVISION_DICT, BUCKET_NAME)
 
     print('running update_current_season')
-    # Run this even if TOTAL_REFRESH == TRUE so it drops data in the config's designated S3 path
     data = update_current_season(CURRENT_BUCKET_PATH, DIVISION_DICT)
 
     df = data_manip(data, season_dim)
